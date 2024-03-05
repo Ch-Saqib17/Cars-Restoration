b350d596e703cefab6508d8add7804d1158cf59a
Ch-Saqib17 <154001347+Ch-Saqib17@users.noreply.github.com>
2024-03-05 23:36:03 +0500

    Update hero5.tsx

--- src/components/Heroes/hero5.tsx
+++ src/components/Heroes/hero5.tsx
@@ -16,40 +16,41 @@ const Hero5 = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-4 px-4 mt-14">
         {/*First Box */}
-
-        <div className="relative">
-          <div className="">
-            <div className="overflow-hidden">
-              <Image
-                src={"/carpainting.jpg"}
-                alt={""}
-                width={400}
-                height={400}
-                className="hover:scale-105 duration-150 rounded-md"
-              />
+        <div className="flex">
+          <div className="relative">
+            <div className="">
+              <div className="overflow-hidden">
+                <Image
+                  src={"/carpainting.jpg"}
+                  alt={""}
+                  width={400}
+                  height={400}
+                  className="hover:scale-105 duration-150 rounded-md"
+                />
+              </div>
             </div>
-          </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
-                Car Painting{" "}
-              </span>
-              <span className="px-8 mb-4">
-                Ut nec interdum cursus torquent elementum fames curae erat
-                phasellus platea euismod{" "}
-              </span>
-              <div className="flex items-center justify-center">
-                <Button>
-                  <span className="mr-2"> Discover More</span>
-                  <MoveRight />
-                </Button>
+            {/*Box */}
+            <div className="absolute z-10 -mt-8 px-4">
+              <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+                <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
+                  Car Painting{" "}
+                </span>
+                <span className="px-8 mb-4">
+                  Ut nec interdum cursus torquent elementum fames curae erat
+                  phasellus platea euismod{" "}
+                </span>
+                <div className="flex items-center justify-center pb-2 mt-2 mb-4">
+                  <Button>
+                    <span className="mr-2"> Discover More</span>
+                    <MoveRight />
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
         {/*Second Box */}
-        <div className="relative mt-40 md:mt-0 lg:mt-0">
+        <div className="relative mt-[250px] md:mt-0 lg:mt-0">
           <div className="">
             <div className="overflow-hidden">
               <Image
@@ -61,16 +62,17 @@ const Hero5 = () => {
               />
             </div>
           </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
+          {/*Box */}
+          <div className="absolute z-10 -mt-8 px-4">
+            <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
                 Body Repair{" "}
               </span>
               <span className="px-8 mb-4">
                 Ut nec interdum cursus torquent elementum fames curae erat
                 phasellus platea euismod{" "}
               </span>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center pb-2 mt-2 mb-4">
                 <Button>
                   <span className="mr-2"> Discover More</span>
                   <MoveRight />
@@ -81,7 +83,7 @@ const Hero5 = () => {
         </div>
 
         {/*Third Box */}
-        <div className="relative mt-40 md:mt-0 lg:mt-0">
+        <div className="relative mt-[250px] md:mt-0 lg:mt-0">
           <div className="">
             <div className="overflow-hidden">
               <Image
@@ -93,16 +95,17 @@ const Hero5 = () => {
               />
             </div>
           </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
+          {/*Box */}
+          <div className="absolute z-10 -mt-8 px-4">
+            <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
                 Parts Replacement{" "}
               </span>
               <span className="px-8 mb-4">
                 Ut nec interdum cursus torquent elementum fames curae erat
                 phasellus platea euismod{" "}
               </span>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center pb-2 mt-2 mb-4">
                 <Button>
                   <span className="mr-2"> Discover More</span>
                   <MoveRight />
@@ -112,7 +115,7 @@ const Hero5 = () => {
           </div>
         </div>
         {/*Fourth Box */}
-        <div className="relative mt-40 md:mt-44 lg:mt-[220px]">
+        <div className="relative mt-[250px] md:mt-44 lg:mt-[220px]">
           <div className="">
             <div className="overflow-hidden">
               <Image
@@ -124,16 +127,17 @@ const Hero5 = () => {
               />
             </div>
           </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
+          {/*Box */}
+          <div className="absolute z-10 -mt-8 px-4">
+            <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
                 Headlight Restoration{" "}
               </span>
               <span className="px-8 mb-4">
                 Ut nec interdum cursus torquent elementum fames curae erat
                 phasellus platea euismod{" "}
               </span>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center pb-2 mt-2 mb-4">
                 <Button>
                   <span className="mr-2"> Discover More</span>
                   <MoveRight />
@@ -143,7 +147,7 @@ const Hero5 = () => {
           </div>
         </div>
         {/*Fifth Box */}
-        <div className="relative mt-40 md:mt-44 lg:mt-[220px]">
+        <div className="relative mt-[250px] md:mt-44 lg:mt-[220px]">
           <div className="">
             <div className="overflow-hidden">
               <Image
@@ -155,16 +159,17 @@ const Hero5 = () => {
               />
             </div>
           </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
+          {/*Box */}
+          <div className="absolute z-10 -mt-8 px-4">
+            <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
                 Pinstripes{" "}
               </span>
               <span className="px-8 mb-4">
                 Ut nec interdum cursus torquent elementum fames curae erat
                 phasellus platea euismod{" "}
               </span>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center pb-2 mt-2 mb-4">
                 <Button>
                   <span className="mr-2"> Discover More</span>
                   <MoveRight />
@@ -174,7 +179,7 @@ const Hero5 = () => {
           </div>
         </div>
         {/*Sixth Box */}
-        <div className="relative mt-40 md:mt-44 lg:mt-[220px]">
+        <div className="relative mt-[250px] md:mt-44 lg:mt-[220px] mb-[100px] md:mb-0 lg:mb-0">
           <div className="">
             <div className="overflow-hidden">
               <Image
@@ -186,16 +191,17 @@ const Hero5 = () => {
               />
             </div>
           </div>
-          <div className="absolute z-10 -mt-8 ml-2">
-            <div className="w-[360px] h-48 md:w-[380px] md:h-48 lg:w-[380px] lg:h-[240px] bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
-              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4">
+          {/*Box */}
+          <div className="absolute z-10 -mt-8 px-4">
+            <div className="w-full h-full  bg-gray-100 shadow-lg flex flex-col justify-center rounded-md items-center">
+              <span className="text-xl md:text-2xl lg:text-2xl font-medium mb-4 pt-8">
                 Spot Painting{" "}
               </span>
               <span className="px-8 mb-4">
                 Ut nec interdum cursus torquent elementum fames curae erat
                 phasellus platea euismod{" "}
               </span>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center pb-2 mt-2 mb-4">
                 <Button>
                   <span className="mr-2"> Discover More</span>
                   <MoveRight />
@@ -210,3 +216,4 @@ const Hero5 = () => {
 };
 
 export default Hero5;
+
