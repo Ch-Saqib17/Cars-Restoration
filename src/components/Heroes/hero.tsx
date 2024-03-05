6d15f345158a1ed1190f70b0b3e64b13644dbab6
Ch-Saqib17 <154001347+Ch-Saqib17@users.noreply.github.com>
2024-03-05 23:34:16 +0500

    Update hero.tsx

--- src/components/Heroes/hero.tsx
+++ src/components/Heroes/hero.tsx
@@ -4,46 +4,47 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div className="relative">
-      <video
-        src="/video.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="w-full"
-      ></video>
-
-      <div className="absolute top-0 lg:p-8 md:p-6 p-10 lg:pl-0 md:pl-0 ml-[70px] lg:mt-10 md:mt-6 mt-4 z-10">
-        <span className="font-extrabold lg:text-8xl md:text-6xl text-4xl text-white">
-          Expertise. <br /> convenience. <br /> reliability.
-        </span>
-      </div>
-      <div className="pl-4">
-        <div className="absolute shadow-lg top-0 bg-gray-200 flex justify-center items-center lg:w-[1230px] md:w-[1080px] w-[380px] h-[260px] lg:px-4  md:px-0 px-0 lg:mt-[600px] md:mt-[400px] mt-[180px] z-10">
-          <section className="flex-row md:flex lg:flex px-6 gap-4">
-            <div className="flex-1">
-              <div className="text-center md:text-start lg:text-start mt-4 md:mt-0 lg:mt-0">
-                <span className=" text-red-500 font-semibold">
-                  Auto Painting and Body Repair
-                </span>
+    <div className="pb-40 md:pb-0 lg:pb-0">
+      <div className=" relative">
+        <div>
+          <div>
+            <video src="/video.mp4" loop autoPlay playsInline muted></video>
+          </div>
+          <div className="absolute z-10 top-10 md:top-52 lg:top-60 text-center md:text-start lg:text-start ml-0 md:ml-4 lg:ml-4 right-0 left-0 text-2xl md:text-4xl lg:text-6xl font-extrabold tracking-tight uppercase text-white">
+            Expertise.
+            <br /> convenience.
+            <br /> reliability.
+          </div>
+          <div className="px-4 z-10 absolute -mt-10">
+            <div className="w-full bg-gray-200 shadow-lg flex flex-col md:flex lg:flex-row justify-center  p-10">
+              <div className="flex flex-col md:flex lg:flex-row gap-6">
+                <div className="flex-1">
+                  <div className="flex flex-col">
+                    <div className="text-red-500 font-bold text-center md:text-start lg:text-start">
+                      Mangata Auto Painting & Body repair
+                    </div>
+                    <div className=" text-center md:text-start lg:text-start text-4xl font-extrabold tracking-tight mt-4">
+                      We are here to help you with your car{" "}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <div className="flex flex-col">
+                    <div className=" text-center md:text-start lg:text-start text-xl">
+                      Our Best Price Guarantee will help make sure you are
+                      getting quality work done at the best price. Saving money
+                      is as easy as book, quote, fix.
+                    </div>
+                    <div className=" text-center md:text-start lg:text-start mt-4">
+                      <Button>
+                        Discover More <MoveRight />
+                      </Button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <span className="font-extrabold  lg:text-4xl md:text-2xl text-lg pb-4  text-center md:text-start lg:text-start">
-                We are here to help you with your car{" "}
-              </span>
             </div>
-            <div className="flex-1 text-center md:text-start lg:text-start  mt-4 md:mt-0 lg:mt-0">
-              <span className="font-semibold">
-                Our Best Price Guarantee will help make sure you are getting
-                quality work done at the best price. Saving money is as easy as
-                book, quote, fix.
-              </span>
-
-              <Button className="mt-6 gap-2 flex items-center ml-20 md:ml-0 lg:ml-0 mb-6 md:mb-0 lg:mb-0">
-                Discover More <MoveRight />
-              </Button>
-            </div>
-          </section>
+          </div>
         </div>
       </div>
     </div>
