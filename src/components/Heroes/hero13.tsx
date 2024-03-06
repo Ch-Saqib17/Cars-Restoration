b1a51e0a74d0d5831f18bcf24a383c0cdd258cec
Ch-Saqib17 <154001347+Ch-Saqib17@users.noreply.github.com>
2024-03-07 00:00:13 +0500

    Update hero13.tsx

--- src/components/Heroes/hero13.tsx
+++ src/components/Heroes/hero13.tsx
@@ -17,12 +17,12 @@ const Hero13 = () => {
       ></video>
 
       <div className="flex-row text-center -mt-[200px] px-[50px] md:-mt-[400px] md:px-[200px]  lg:-mt-[500px] lg:px-[300px]">
-        <div className="mb-2 md:mb-3 lg:mb-10">
-          <span className="text-lg md:text-2xl lg:text-5xl font-extrabold text-white">
+        <div className="mb-1 md:mb-3 lg:mb-10">
+          <span className=" text-xm md:text-2xl lg:text-5xl font-extrabold text-white">
             Discount up to 30% only this month for new member
           </span>
         </div>
-        <div className="mb-2 md:mb-3 lg:mb-10">
+        <div className="mb-1 md:mb-3 lg:mb-10">
           <span className="text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
